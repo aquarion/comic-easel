637039645b104a7230ca2c36520bd1f754b2eef8
Nicholas Avenell <nicholas@istic.net>
2024-10-11 10:33:28 +0000

    Refactor code for php linting

--- js/casthoverfunc.js
+++ js/casthoverfunc.js
@@ -1,17 +1,21 @@
-jQuery(document).ready(function($){
+jQuery(document).ready(
+    function ($) {
     
-    $(".casthover-hovercard-hook").hover(
-  function() {
-	    $pposl = $(this).offset().left;
-	    $ppost = $(this).offset().top;
-	    $pposl = $pposl - 50;
-	    $(this).find('div.casthover-hovercard').hide().css('visibility','visible').fadeIn('fast');
-	    $(this).find('div.casthover-hovercard').css('left',$pposl);
-	},
-	function() {
-	    $(this).find('div.casthover-hovercard').fadeOut('slow', function () {
-		$(this).show().css('visibility','hidden');
-	    })
-	}
-    );
-})
+        $(".casthover-hovercard-hook").hover(
+            function () {
+                $pposl = $(this).offset().left;
+                $ppost = $(this).offset().top;
+                $pposl = $pposl - 50;
+                $(this).find('div.casthover-hovercard').hide().css('visibility','visible').fadeIn('fast');
+                $(this).find('div.casthover-hovercard').css('left',$pposl);
+            },
+            function () {
+                $(this).find('div.casthover-hovercard').fadeOut(
+                    'slow', function () {
+                        $(this).show().css('visibility','hidden');
+                    }
+                )
+            }
+        );
+    }
+)
--- js/multicomic.js
+++ js/multicomic.js
@@ -1,33 +1,39 @@
-jQuery(document).ready(function () {
-    var $ = jQuery, buttonpanel, comicpanes;
+jQuery(document).ready(
+    function () {
+        var $ = jQuery, buttonpanel, comicpanes;
     
-    // Cache our panes
-    comicpanes = $("#comic .comicpane");
+        // Cache our panes
+        comicpanes = $("#comic .comicpane");
     
-    if (comicpanes.length > 1) {
-        // Make our button panel
-        buttonpanel = $('<div id="buttonpanel"></div>');
+        if (comicpanes.length > 1) {
+            // Make our button panel
+            buttonpanel = $('<div id="buttonpanel"></div>');
         
-        // Loop over our panes
-        comicpanes.each(function (i) {
-            var button;
+            // Loop over our panes
+            comicpanes.each(
+                function (i) {
+                    var button;
 
-            // Make a button for it
-            button = $('<button id="show-' + (i+1) + '" class="comicbutton">' + (i+1) + '</button>');
+                    // Make a button for it
+                    button = $('<button id="show-' + (i+1) + '" class="comicbutton">' + (i+1) + '</button>');
             
-            // Make the button hide all the comics and show this one
-            button.click(function (e) {
-                comicpanes.hide().eq(i).show();
-            });
+                    // Make the button hide all the comics and show this one
+                    button.click(
+                        function (e) {
+                            comicpanes.hide().eq(i).show();
+                        }
+                    );
             
-            // Put the button in the panel
-            buttonpanel.append(button);
-        });
+                    // Put the button in the panel
+                    buttonpanel.append(button);
+                }
+            );
         
-        // Add the panel to the main comic pane
-        buttonpanel.appendTo("#comic");
+            // Add the panel to the main comic pane
+            buttonpanel.appendTo("#comic");
         
-        // Hide all but the first strip
-        comicpanes.hide().eq(0).show();
-    } // if (comicpanes.length > 1)
-});
+            // Hide all but the first strip
+            comicpanes.hide().eq(0).show();
+        } // if (comicpanes.length > 1)
+    }
+);
